ba4b73cfd4a7c1dca713caa3900bee2149c2996d
m2pham <mpham@ufl.edu>
2014-10-05 14:32:37 -0400

    add entity manager

---
+++ YellowMamba/Managers/EntityManager.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using YellowMamba.Entities;
+using YellowMamba.Players;
+
+namespace YellowMamba.Managers
+{
+    public class EntityManager
+    {
+        public List<Entity> Entities { get; private set; }
+
+        public EntityManager()
+        {
+            Entities = new List<Entity>();
+        }
+
+        public void LoadContent()
+        {
+            foreach (Entity entity in Entities)
+            {
+                entity.LoadContent();
+            }
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            foreach (Entity entity in Entities)
+            {
+                entity.Update(gameTime);
+            }
+        }
+
+        public void Draw(GameTime gameTime, SpriteBatch spriteBatch)
+        {
+            foreach (Entity entity in Entities)
+            {
+                entity.Draw(gameTime, spriteBatch);
+            }
+        }
+
+        public Player GetPlayer(PlayerIndex playerIndex)
+        {
+            return Entities.Find(x => x.PlayerIndex == playerIndex);
+        }
+    }
+}
